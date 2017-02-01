01ad2144a428e170f05e7b15555b794108cffb8b
broskh <alessio.scheri@gmail.com>
2017-02-01 15:01:40 +0100

    Prima bozza della classe main.

---
+++ src/Java_eCommerce.java
@@ -0,0 +1,16 @@
+import grafica.JeCommerceFrame;
+
+/**
+ * La classe Java_eCommerce è la classe principale che contiene il
+ * metodo main per l'esecuzione del programma.
+ * ---------------->CONTINUARE<---------------------
+ * 
+ * @author Alessio Scheri
+ * @version 1.0
+ */
+public class Java_eCommerce {
+	public static void main(String[] args) {
+		JeCommerceFrame jeCommerceFrame = new JeCommerceFrame();
+		jeCommerceFrame.setVisible(true);
+	}
+}
