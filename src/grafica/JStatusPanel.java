6789c136c9ab751b90219180cc03096d2324f68f
broskh <alessio.scheri@gmail.com>
2017-05-02 11:55:07 +0200

    Realizzato JStatusPanel.

---
+++ src/grafica/JStatusPanel.java
@@ -0,0 +1,104 @@
+package grafica;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+import utenza.Utente;
+
+public class JStatusPanel extends JPanel{
+	private static final long serialVersionUID = -1821269487258614928L;
+	private String nome;
+	private String cognome;
+	private String tipo;
+
+	private JLabel jNameLabel;
+	private JLabel jTypeLabel;
+	private JLabel jAuthorLabel;
+
+	private static final String intestazioneNomeUtente = "Nome utente: ";
+	private static final String intestazioneTipoUtente = "Tipo utente: ";
+	private static final String intestazioneAutore = "Autore: ";
+	private static final String autore = "Alessio Scheri";
+	private static final int altezza = 22;
+	
+	public JStatusPanel (String nome, String cognome, String tipo) {
+		//inizializzo gli attributi contenenti i valori da utilizzare
+		this.nome = nome;
+		this.cognome = cognome;
+		this.tipo = tipo;
+		//setto la grafica del panel
+		this.setPreferredSize(new Dimension(this.getWidth(), altezza));
+		this.setLayout(new GridLayout(1,3));
+		//setto i vari componenti
+		this.jNameLabel = new JLabel();
+		this.jTypeLabel = new JLabel();
+		this.jAuthorLabel = new JLabel();
+		this.jNameLabel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		this.jNameLabel.setHorizontalAlignment(JLabel.CENTER);
+		this.jTypeLabel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		this.jTypeLabel.setHorizontalAlignment(JLabel.CENTER);
+		this.jAuthorLabel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		this.jAuthorLabel.setHorizontalAlignment(JLabel.CENTER);
+		//aggiorno le stringhe delle label
+		this.updateLabels();
+		//aggiungo le label al panel
+		this.add(this.jNameLabel);
+		this.add(this.jTypeLabel);
+		this.add(this.jAuthorLabel);
+	}
+	
+	public JStatusPanel () {
+		this ("", "", "");
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public void setCognome(Utente utente) {
+		this.cognome = utente.getCognome();
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setNome(Utente utente) {
+		this.nome = utente.getNome();
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setTipo(Utente utente) {
+		if (utente.isAmministratore()) {
+			this.tipo = "Amministratore";
+		}
+		else {
+			this.tipo = "Cliente";
+		}
+	}
+	
+	public void updateLabels () {
+		this.jNameLabel.setText(JStatusPanel.intestazioneNomeUtente + this.nome + " " + this.cognome);
+		this.jTypeLabel.setText(JStatusPanel.intestazioneTipoUtente + this.tipo);
+		this.jAuthorLabel.setText(JStatusPanel.intestazioneAutore + JStatusPanel.autore);
+	}
+}
--- src/grafica/JeCommerceFrame.java
+++ src/grafica/JeCommerceFrame.java
@@ -2,7 +2,6 @@ package grafica;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
 
 import javax.swing.JFrame; 
 import javax.swing.JPanel;
@@ -10,17 +9,17 @@ import javax.swing.JPanel;
 public class JeCommerceFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static final String titolo = "Java-eCommerce";
-	private static final int altezzaDefault = 600;
-	private static final int larghezzaDefault = 800;
+//	private static final int altezzaDefault = 600;
+//	private static final int larghezzaDefault = 800;
 	
 	private JPanel jMenuPanel;
 	private JPanel jControlPanel;
 	private JPanel jContentPanel;
-	private JPanel jStatusPanel;
+	private JStatusPanel jStatusPanel;
  
 	public JeCommerceFrame () {
 		super (JeCommerceFrame.titolo);
-		this.setSize(JeCommerceFrame.larghezzaDefault, JeCommerceFrame.altezzaDefault);
+//		this.setSize(JeCommerceFrame.larghezzaDefault, JeCommerceFrame.altezzaDefault);
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
@@ -32,9 +31,7 @@ public class JeCommerceFrame extends JFrame {
 		this.jControlPanel.setSize(1920, 30); //togliere
 		this.jControlPanel.setBackground(new Color(255, 255, 255)); //togliere
 		this.jContentPanel = new JUserContentPanel();
-		this.jStatusPanel = new JPanel ();
-		this.jStatusPanel.setSize(1920, 10); //togliere
-		this.jStatusPanel.setBackground(new Color(0, 0, 0)); //togliere
+		this.jStatusPanel = new JStatusPanel ();
 
 		this.add(this.jMenuPanel, BorderLayout.PAGE_START);
 		this.add(this.jControlPanel, BorderLayout.NORTH);
