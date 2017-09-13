2e6a7ca31c7aca0f9d5251ae724aa529736fb5ec
broskh <alessio.scheri@gmail.com>
2017-09-13 15:43:02 +0200

    Migliorato script bash.

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 javac $(find src/ -regex '.*.java')
-java -cp src Java_eCommerce
+if [ $? -eq 0 ]
+then
+	echo "***********************"
+  	echo "Compilazione completata."
+	echo "***********************"
+	echo "Avvio del programma."
+	echo "***********************"
+	java -cp src Java_eCommerce
+fi
